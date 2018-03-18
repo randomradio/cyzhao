7c083d567e815695572f4786cff73f0f03229990
chenyangzhao <chzhao8904@gmail.com>
2018-03-17 19:29:57 -0700

    update javascript, main.css

---
+++ themes/kong/static/js/main.js
@@ -0,0 +1,18 @@
+function blog() {
+    
+    function init() {
+        var deltaY = 0;
+        document.addEventListener('scroll', function(e) {
+            console.log(e);            
+        });
+    }
+
+    /** init blog javascript */
+    // init();
+
+    return {
+            }
+}
+
+var Blog = blog();
+
