e43eb0f6d1a289ecff1b9479014b33efbaaf345f
chenyangzhao <chzhao8904@gmail.com>
2018-03-28 09:41:23 -0700

    adding webserver, gorm, and readinglist feature

--- webserver/main.go
+++ webserver/main.go
@@ -2,22 +2,67 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+
+	"github.com/PuerkitoBio/goquery"
 )
+
+type ReadItem struct {
+	gorm.Model
+	URL   string
+	Title string
+}
 
 func addUlr(w http.ResponseWriter, r *http.Request) {
 
 	url := r.FormValue("url")
 
 	resp, err := http.Get(url)
-	if err != nil {
-		// handle error
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
 
-	w.Write(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var title string
+
+	// Find the review items
+	doc.Find("title").Each(func(i int, s *goquery.Selection) {
+		if title == "" {
+			title = s.Text()
+		}
+	})
+
+	fmt.Println(title)
+
+	addDBItem(url, title)
+
+	return
+}
+
+func addDBItem(url string, title string) {
+	db, err := gorm.Open("sqlite3", "item.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&ReadItem{})
+
+	// Create
+	db.Create(&ReadItem{URL: url, Title: title})
 }
 
 func main() {
